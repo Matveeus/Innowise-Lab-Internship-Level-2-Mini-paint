9f986da2a67187f9e3617edcfabc9705d52311e4
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-05-24 10:33:29 +0200

    eslint is configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
     },
-    extends: ['eslint-config-airbnb-base'],
-    parserOptions: {
-        project: './tsconfig.json',
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
     },
-    overrides: [
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
         {
-            files: ['src/**/*.js', 'src/**/*.jsx'],
-            rules: {
-                "react/jsx-filename-extension": [
-                    1,
-                    { "extensions": [".js", ".jsx"] }
-                ]
-            }
+            "files": ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
         }
     ],
-    plugins: [
-        'react',
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
     ],
-};
+    "rules": {
+    }
+}
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider }  from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
